31e89446befed66c8affc6896591ce35c442b4d1
John Raber <jrab224@gmail.com>
2018-05-17 21:17:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   var line = "The line is currently: ";
-  var x = 1;
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    for (x; x < katzDeliLine.length; x++) {
+    for (let x = 1; x < katzDeliLine.length; x++) {
       line = line + `${x}. ${katzDeliLine[x - 1]}, `;
     }
     line = line + `${katzDeliLine.length}. ${katzDeliLine[katzDeliLine.length - 1]}`;
